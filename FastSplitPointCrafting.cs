81abadf12e6174b093912cb63516fbb5eea048cb
tbrowne03 <tbrowne03@gmail.com>
2023-11-25 23:45:41 -0700

    Updated to use built in ScriptHook methods, instead of hard-coded function calls. Added functionality for Rifles. Fixed a bug where the player could convert more ammo than the gun would hold.

--- FastSplitPointCrafting.cs
+++ FastSplitPointCrafting.cs
@@ -9,41 +9,42 @@ namespace RDR2
 {
     public class FastSplitPointCrafting : Script
     {
-        //Member Variables
+        // Variables
         private bool mGiveAmmo = false;
-        private int mAmmoIncrement;
+        private int mSpecifiedAmmoIncrement;
+        private int mActualAmmoIncrement;
         private Ped mPlayerPed;
         private Keys mKeyBinding;
 
-        private int mAmmoAddType;
-        private int mAmmoRemoveType;        
+        private eAmmoType mAmmoAddType;
+        private eAmmoType mAmmoRemoveType;        
 
-        private List<int> validAmmoTypes = new List<int>();
-        private Dictionary<int, int> splitAmmoLookup = new Dictionary<int, int>();
+        private List<eAmmoType> validAmmoTypes = new List<eAmmoType>();
+        private Dictionary<eAmmoType, eAmmoType> splitAmmoLookup = new Dictionary<eAmmoType, eAmmoType>();
 
-        /// <summary>
-        /// Constructor
-        /// </summary>
+        // Constructor
         public FastSplitPointCrafting()
         {
-            //Initialize Variables
+            // Initialize Variables
             mPlayerPed = Game.Player.Character;
 
-            //Initialize Listeners
+            // Initialize Listeners
             KeyDown += OnKeyDown;
             Tick += OnTick;
 
-            //Initialize Valid Ammo Types
-            validAmmoTypes.Add(ConvertEnumToInt("AMMO_REVOLVER"));
-            validAmmoTypes.Add(ConvertEnumToInt("AMMO_PISTOL"));
-            validAmmoTypes.Add(ConvertEnumToInt("AMMO_REPEATER"));
+            // Initialize Valid Ammo Types
+            validAmmoTypes.Add(eAmmoType.Revolver);
+            validAmmoTypes.Add(eAmmoType.Pistol);
+            validAmmoTypes.Add(eAmmoType.Repeater);
+            validAmmoTypes.Add(eAmmoType.Rifle);
 
-            //Populate a dictionary to relate ammo type/ split point ammo type
-            splitAmmoLookup.Add(ConvertEnumToInt("AMMO_REVOLVER"), ConvertEnumToInt("AMMO_REVOLVER_SPLIT_POINT"));
-            splitAmmoLookup.Add(ConvertEnumToInt("AMMO_PISTOL"),   ConvertEnumToInt("AMMO_PISTOL_SPLIT_POINT"));
-            splitAmmoLookup.Add(ConvertEnumToInt("AMMO_REPEATER"), ConvertEnumToInt("AMMO_REPEATER_SPLIT_POINT"));
+            // Populate a dictionary to relate ammo type/ split point ammo type
+            splitAmmoLookup.Add(eAmmoType.Revolver, eAmmoType.RevolverSplitPoint);
+            splitAmmoLookup.Add(eAmmoType.Pistol,   eAmmoType.PistolSplitPoint);
+            splitAmmoLookup.Add(eAmmoType.Repeater, eAmmoType.RepeaterSplitPoint);
+            splitAmmoLookup.Add(eAmmoType.Rifle,    eAmmoType.RifleSplitPoint);
 
-            //Read the config file
+            // Read the config file
             bool keyConfigSuccess = false;
             bool ammoIncrementSuccess = false;
 
@@ -54,124 +55,105 @@ namespace RDR2
                 lines = File.ReadAllLines(filePath);
             } catch (Exception)
             {
-                //Do nothing
+                // Do nothing
             }
 
             foreach (string line in lines)
             {
                 string[] substring = line.Split('=');
 
-                //Set the KeyBinding
+                // Set the KeyBinding
                 if (substring.Length == 2 &&
                     substring[0] == "fastAmmoCraftingKey")
                 {
                     keyConfigSuccess = Enum.TryParse(substring[1], out mKeyBinding);
+                    continue;
                 }
                 
-                //Set the Increment Amount
+                // Set the Increment Amount
                 if (substring.Length == 2 &&
                     substring[0] == "fastAmmoCraftingAmount")
                 {
-                    ammoIncrementSuccess = int.TryParse(substring[1], out mAmmoIncrement);
+                    ammoIncrementSuccess = int.TryParse(substring[1], out mSpecifiedAmmoIncrement);
+                    continue;
                 }
-
-                break;
             }
 
-            //Set default values if the config. file could not be loaded successfully
+            // Set default values if the config file could not be loaded successfully
             if (!keyConfigSuccess)
             {
                 mKeyBinding = Keys.K;
             }
 
             if (!ammoIncrementSuccess ||
-                mAmmoIncrement < 0)
+                mSpecifiedAmmoIncrement < 0)
             {
-                mAmmoIncrement = 25;
+                mSpecifiedAmmoIncrement = 25;
             }
         }
 
-        /// <summary>
-        /// Listener for Key Press Events
-        /// </summary>
-        /// <param name="sender">The event sender</param>
-        /// <param name="e">The KeyEventArgs</param>
+        // Listener for Key Press Events
         private void OnKeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == mKeyBinding)
             {
-                //If player is crouching
-                //GET_PED_CROUCH_MOVEMENT: Hash._0xD5FE956C70FF370B
-                if (Function.Call<bool>(Hash._0xD5FE956C70FF370B, mPlayerPed))
+                // If player is crouching
+                if (PED.GET_PED_CROUCH_MOVEMENT(mPlayerPed))
                 {
-                    //Get the current weapon
-                    //_GET_SELECTED_PED_WEAPON: Hash._0x8425C5F057012DAB
-                    int currentPlayerWeapon = (int)Function.Call<Hash>(Hash._0x8425C5F057012DAB, mPlayerPed);
+                    // Get the default ammo type for the currently held weapon
+                    eAmmoType defaultAmmoType = mPlayerPed.Weapons.Current.DefaultAmmoType;
 
-                    //Get the ammo type for the weapon
-                    //_GET_AMMO_TYPE_FOR_WEAPON: Hash._0x5C2EA6C44F515F34
-                    int defaultAmmoType = (int)Function.Call<Hash>(Hash._0x5C2EA6C44F515F34, currentPlayerWeapon);
-
-                    //If the weapon uses a valid ammo type
+                    // If the weapon uses a valid ammo type
                     if (validAmmoTypes.Contains(defaultAmmoType))
                     {
                         mAmmoAddType = splitAmmoLookup[defaultAmmoType];
-                        mAmmoRemoveType = defaultAmmoType;  
+                        mAmmoRemoveType = defaultAmmoType;
 
-                        //If you have enough ammo
-                        if (GetAmmoCount(mAmmoRemoveType) >= mAmmoIncrement)
+                        // Check how much ammo should be crafted. Normally, this is set by mSpecifiedAmmoIncrement. However, sometimes this amount would craft too much ammo, and we should craft less.
+                        // Note: The max split point ammo a gun can hold is the regular ammo max / 2
+                        if ((GetAmmoCount(mAmmoAddType) + mSpecifiedAmmoIncrement) > (mPlayerPed.Weapons.Current.MaxAmmo / 2))
+                        {
+                            mActualAmmoIncrement = (mPlayerPed.Weapons.Current.MaxAmmo / 2) - GetAmmoCount(mAmmoAddType);
+                        } else
+                        {
+                            mActualAmmoIncrement = mSpecifiedAmmoIncrement;
+                        }
+
+                        // If you have enough ammo 
+                        if (GetAmmoCount(mAmmoRemoveType) >= mActualAmmoIncrement)
                         {
                             mGiveAmmo = true;
                         }
                         else
                         {
-                            RDR2.UI.Screen.ShowSubtitle($"Could not craft split point ammo. Insufficient materials.");
+                            RDR2.UI.Screen.DisplaySubtitle($"Could not craft split point ammo. Insufficient materials.");
                         }
                     }
                 }
             }
         }
 
-        /// <summary>
-        /// Listener for tick events
-        /// </summary>
-        /// <param name="sender">The object sender</param>
-        /// <param name="e">The EventArgs</param>
+        // Listener for tick events
         private void OnTick(object sender, EventArgs e)
         {
             if (mGiveAmmo)
             {
-                // _GIVE_AMMO_TO_PED: Hash._0x106A811C6D3035F3
-                Function.Call(Hash._0x106A811C6D3035F3, mPlayerPed, mAmmoAddType, mAmmoIncrement, 0x2CD419DC);
+                // Add split point ammo to ped
+                WEAPON._ADD_AMMO_TO_PED_BY_TYPE(mPlayerPed, (uint)mAmmoAddType, mActualAmmoIncrement, (uint)eAddItemReason.Default);
+                
+                // Remove regular ammo from ped
+                WEAPON._REMOVE_AMMO_FROM_PED_BY_TYPE(mPlayerPed, (uint)mAmmoRemoveType, mActualAmmoIncrement, (uint)eRemoveItemReason.Used);
 
-                //_REMOVE_AMMO_FROM_PED_BY_TYPE: Hash._0xB6CFEC32E3742779
-                Function.Call(Hash._0xB6CFEC32E3742779, mPlayerPed, mAmmoRemoveType, mAmmoIncrement, 0x2188E0A3);
-
-                RDR2.UI.Screen.ShowSubtitle($"Added {mAmmoIncrement} split point ammo to inventory.");
-
+                RDR2.UI.Screen.DisplaySubtitle($"Added {mActualAmmoIncrement} split point ammo to inventory.");
                 mGiveAmmo = false;
             }
         }
 
-        /// <summary>
-        /// A generic method to convert an enum to its corresponding int
-        /// </summary>
-        /// <param name="convertEnum"></param>
-        /// <returns>An int representing the enum</returns>
-        private int ConvertEnumToInt(string convertEnum)
+        // Gets the Players total ammo of a given type
+        private int GetAmmoCount(eAmmoType pAmmoType)
         {
-            int output = (int)Function.Call<Hash>(Hash.GET_HASH_KEY, convertEnum);
-            return output;
-        }
-
-        /// <summary>
-        /// Gets the Players total ammo of a given type
-        /// </summary>
-        /// <param name="pAmmoType">The type of ammo</param>
-        /// <returns>The amount of the ammo type currently in the players inventory</returns>
-        private int GetAmmoCount(int pAmmoType)
-        {
-            int ammoCount = Function.Call<int>(Hash.GET_PED_AMMO_BY_TYPE, mPlayerPed, pAmmoType);
+            int ammoCount = WEAPON.GET_PED_AMMO_BY_TYPE(mPlayerPed, (uint)pAmmoType);
+            
             return ammoCount;
         }
     }
